55b29118ba7976c26a382c9916b878a72c6611d1
Seth Dimmer <sethdimmer1@gmail.com>
2023-05-26 14:02:16 -0400

    initial commit

---
+++ src/components/Nav.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import MoviFlixLogo from "../assets/Untitled design (1).png"
+
+
+function Nav() {
+  return (
+    <nav>
+        <div className="nav__container">
+            <a href='/'>
+            <img src={MoviFlixLogo} alt='' className='logo' />
+            </a>
+            <ul className='nav__links'>
+                <li className='nav__list'>
+                    <a  href='/' className='nav__link'>
+                        Home
+                    </a>
+                </li>
+                <li className='nav__list'>
+                    <a href='/' className='nav__link--btn'>
+                        Browse Movies
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+  );
+}
+
+export default Nav
